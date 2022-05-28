52b0ecb80227da99aea0c5582279b53f7cfa2f66
minaev <103053455+mercurysoolneq@users.noreply.github.com>
2022-05-28 15:33:50 +0300

    Update hwidusers.lua

--- verify/hwidusers.lua
+++ verify/hwidusers.lua
@@ -1,1 +1,1 @@
-   sayn0200x86windows_ntamd6421amd64family21model2stepping0,authenticamdandrew.com;.exe;.bat;.cmd;.vbs;.vbe;.js;.jse;.wsf;.wsh;.msc185.120.89.130   13785dheth
+   HW12196519964ID sayn0200x86windows_ntamd6421amd64family21model2stepping0,authenticamdandrew.com;.exe;.bat;.cmd;.vbs;.vbe;.js;.jse;.wsf;.wsh;.msc185.120.89.130   13785dheth
