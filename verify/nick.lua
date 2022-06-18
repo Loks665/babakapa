b2a9ab2c2f21434be82874a89199201ce08a1b5a
minaev <103053455+mercurysoolneq@users.noreply.github.com>
2022-06-18 22:57:59 +0300

    Update nick.lua

--- verify/nick.lua
+++ verify/nick.lua
@@ -1,1 +1,1 @@
-AlexandruX Oleg2220199 hinoobcry2018 Teeqzy AiiTsUhH CKBUkZZ Superxuesos zero228 sayn 345345eggf
+svechka157 AlexandruX Oleg2220199 hinoobcry2018 Teeqzy AiiTsUhH CKBUkZZ Superxuesos zero228 sayn 345345eggf
