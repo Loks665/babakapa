25952e33053fc67cc1dfc23e8dff4d31c597fd97
minaev <103053455+mercurysoolneq@users.noreply.github.com>
2022-06-28 17:58:48 +0300

    Update playernick.lua

--- verify/playernick.lua
+++ verify/playernick.lua
@@ -1,1 +1,1 @@
-saynAsistant123 zero228mercurynesmotrisud F8te 1loveles Kileas svechka157 AlexandruX Oleg2220199 hinoobcry2018 AiiTsUhH CKBUkZZ Superxuesos  sayn 345345eggf
+saynAsistant123 zero228mercurynesmotrisuda F8te 1loveles Kileas svechka157 AlexandruX Oleg2220199 hinoobcry2018 AiiTsUhH CKBUkZZ Superxuesos  sayn 345345eggf
