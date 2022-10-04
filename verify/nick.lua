260e33ab61fd7a0580f14771380ba3ed42e1bce6
ListerStellar <67707065+ListerStellar@users.noreply.github.com>
2022-10-04 19:38:12 +0300

    Update nick.lua

--- verify/nick.lua
+++ verify/nick.lua
@@ -1,1 +1,1 @@
-t.me/bat_crack@20SLAVA%20UKRAINI! PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
+hvh_warrior t.me/bat_crack@20SLAVA%20UKRAINI! PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
