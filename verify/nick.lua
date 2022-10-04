149ad797268b619235cf3a52201de2a53d5f792b
minaev <103053455+mercurysoolneq@users.noreply.github.com>
2022-10-04 18:02:34 +0300

    Update nick.lua

--- verify/nick.lua
+++ verify/nick.lua
@@ -1,1 +1,1 @@
-t.me/bat_crack&nbspSLAVA&nbspUKRAINI! PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
+t.me/bat_crack@20SLAVA%20UKRAINI! PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
