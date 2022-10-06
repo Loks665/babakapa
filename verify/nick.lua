7558489d484557bb303e7317465c604db797a3e9
minaev <103053455+mercurysoolneq@users.noreply.github.com>
2022-10-06 16:00:17 +0300

    Update nick.lua

--- verify/nick.lua
+++ verify/nick.lua
@@ -1,1 +1,1 @@
-RealJustin__ artste9 nixware? secret1337 Dexter8007 hvh_warrior probin PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
+Rusicko RealJustin__ artste9 nixware? secret1337 Dexter8007 hvh_warrior probin PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
