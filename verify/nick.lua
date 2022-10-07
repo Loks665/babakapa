122f9dbdccaf5568bea2e922f7d3236858071b87
ListerStellar <67707065+ListerStellar@users.noreply.github.com>
2022-10-07 18:08:32 +0300

    Update nick.lua

--- verify/nick.lua
+++ verify/nick.lua
@@ -1,1 +1,1 @@
-kewaq Fastik5580 Rusicko RealJustin__ artste9 nixware? secret1337 Dexter8007 hvh_warrior probin PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
+HAARD kewaq Fastik5580 Rusicko RealJustin__ artste9 nixware? secret1337 Dexter8007 hvh_warrior probin PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
