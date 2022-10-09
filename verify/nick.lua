faa05d3cdf083fba4546b18152b367ba2d0bf114
m1ndexx <114593839+m1ndexx@users.noreply.github.com>
2022-10-09 20:16:17 +0300

    Update nick.lua

--- verify/nick.lua
+++ verify/nick.lua
@@ -1,1 +1,1 @@
-ardabey05 koncwer DaniilCh a2ndrei13 HAARD kewaq Fastik5580 Rusicko RealJustin__ artste9 nixware? secret1337 Dexter8007 hvh_warrior probin PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
+kekpank ardabey05 koncwer DaniilCh a2ndrei13 HAARD kewaq Fastik5580 Rusicko RealJustin__ artste9 nixware? secret1337 Dexter8007 hvh_warrior probin PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
