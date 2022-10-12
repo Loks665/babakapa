d87b3b0cb3136f0a13a0dc442259922063475355
minaev <103053455+mercurysoolneq@users.noreply.github.com>
2022-10-12 20:19:58 +0300

    Update nick.lua

--- verify/nick.lua
+++ verify/nick.lua
@@ -1,1 +1,1 @@
- kuvin sashabez1011 dembel nixware? Xpipe Sashabez1011 Yud3x kekpank ardabey05 koncwer DaniilCh a2ndrei13 HAARD kewaq Fastik5580 Rusicko RealJustin__ artste9  secret1337 Dexter8007 hvh_warrior probin PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
+chettopg kuvin sashabez1011 dembel nixware? Xpipe Sashabez1011 Yud3x kekpank ardabey05 koncwer DaniilCh a2ndrei13 HAARD kewaq Fastik5580 Rusicko RealJustin__ artste9  secret1337 Dexter8007 hvh_warrior probin PEACEDUKE1 ClustyBTW Soldyou voxe Csgohile14 hinoobcry2018 Kasikek sayn 345345eggf
