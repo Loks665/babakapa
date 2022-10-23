1a2eb2254336176f2615094a8ac44bf69e69031f
m1ndexx <114593839+m1ndexx@users.noreply.github.com>
2022-10-23 20:24:05 +0300

    Update nick.lua

--- verify/nick.lua
+++ verify/nick.lua
@@ -1,1 +1,1 @@
-sayn ebik1 testnick6 testnick4 testnick3 testnick1 37485v432 sadfgq2 smert hehe123 turbo_huesos artste9 sayn dasd
+Inky sayn ebik1 testnick6 testnick4 testnick3 testnick1 37485v432 sadfgq2 smert hehe123 turbo_huesos artste9 sayn dasd
