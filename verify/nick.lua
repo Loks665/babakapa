2fa6df631b0923a11bbb00c38af2602a5d050d87
minaev <103053455+mercurysoolneq@users.noreply.github.com>
2022-10-30 13:51:22 +0300

    Delete new file via GitHub API

--- verify/nick.lua
+++
@@ -1,1 +0,0 @@
-sayn clusty Rust21 Rezax fatalitybro dembel ryon12341 clusty ryon12341 misha21 ryon12341 sayn sayn sayn ItYeet dayn1232 Loks sayn zxcoutwhite 32132131 mapl7331 s0l0viev07 fwfwef zxcoutwhite Inky sayn artste9 sayn dasd
