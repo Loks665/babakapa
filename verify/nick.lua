9c02c79bd55afd08ba3557cc9b95e088a1b2907e
m1ndexx <114593839+m1ndexx@users.noreply.github.com>
2022-11-08 14:28:27 +0300

    Update nick.lua

--- verify/nick.lua
+++ verify/nick.lua
@@ -1,1 +1,1 @@
-Yud3x clusty Fn_Letwik kekpank MineFort520 MineFort520 reverthvh ItYeet exampleofilya ukhkhan2007 Xpipe sculex sssa sayn clusty Rust21 Rezax fatalitybro dembel ryon12341 misha21 dayn1232 Loks zxcoutwhite 32132131 mapl7331 s0l0viev07 fwfwef zxcoutwhite Inky artste9 dasd
+entry Yud3x clusty Fn_Letwik kekpank MineFort520 MineFort520 reverthvh ItYeet exampleofilya ukhkhan2007 Xpipe sculex sssa sayn clusty Rust21 Rezax fatalitybro dembel ryon12341 misha21 dayn1232 Loks zxcoutwhite 32132131 mapl7331 s0l0viev07 fwfwef zxcoutwhite Inky artste9 dasd
